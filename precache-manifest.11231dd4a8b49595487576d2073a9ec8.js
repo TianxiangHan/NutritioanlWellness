802a4b529c8d378846984497d07f6f8dec666f18
TianxiangHan <hantxmsg@gmail.com>
2022-03-30 21:29:27 +1300

    Updates

---
+++ precache-manifest.11231dd4a8b49595487576d2073a9ec8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8236f76591a88594c8879f2b60927fc4",
+    "url": "/NutritionalWellness/index.html"
+  },
+  {
+    "revision": "e78cdd66ef00ce7a1df3",
+    "url": "/NutritionalWellness/static/css/2.1292276e.chunk.css"
+  },
+  {
+    "revision": "e78cdd66ef00ce7a1df3",
+    "url": "/NutritionalWellness/static/js/2.58f099f1.chunk.js"
+  },
+  {
+    "revision": "4b0fb94a38b65ab60472",
+    "url": "/NutritionalWellness/static/js/main.21b7f4a6.chunk.js"
+  },
+  {
+    "revision": "3eb7d017a5ae4d015891",
+    "url": "/NutritionalWellness/static/js/runtime-main.82f0389d.js"
+  }
+]);
